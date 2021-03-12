6323674721694ef5d1d585a926ee67914b074a52
CandidDisk <marktajima@me.com>
2021-03-12 14:48:35 +0800

    Pi - Arduino I/O test
    Simple test case for serial communication & also to help build serialInter & arduinoIO, & also test Arduino CLI wrapper

---
+++ tests/arduinoIO.ino
@@ -0,0 +1,77 @@
+
+const byte numChars = 64;
+char receivedChars[numChars];
+
+boolean newData = false;
+
+
+//===============
+
+void setup() {
+    Serial.begin(115200);
+
+    pinMode(LED_BUILTIN, OTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    Serial.println("<Arduino is ready>");
+}
+
+//===============
+
+void loop() {
+    recvWithStartEndMarkers();
+    replyToPython();
+}
+
+//===============
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+//===============
+
+void replyToPython() {
+    if (newData == true) {
+        Serial.print("<This just in ... ");
+        Serial.print(receivedChars);
+        Serial.print("   ");
+        Serial.print(millis());
+        Serial.print('>');
+            // change the state of the LED everytime a reply is sent
+        digitalWrite(LED_BUILTIN, ! digitalRead(LED_BUILTIN));
+        newData = false;
+    }
+}
