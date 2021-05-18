026b6cd8ccdd0d24adb545bd74866f0160924570
CandidDisk <marktajima@me.com>
2021-05-18 18:27:39 +0800

    New sample for compareImg test

--- piGantry/imageProcess/cameraFunc.py
+++ piGantry/imageProcess/cameraFunc.py
@@ -34,7 +34,7 @@ def preProcImg(img):
     return image
 
 def retContour(img, minArea, maxArea, exemptArea, file):
-    #image2 = np.zeros((2448,3264,3),np.uint8)
+    image2 = np.zeros((2448,3264,3),np.uint8)
 
     contours, hierarchy = cv2.findContours(
         img,
@@ -60,16 +60,16 @@ def retContour(img, minArea, maxArea, exemptArea, file):
                 #intensity = img[int(contourMoment['m10'] / contourMoment['m00'])][int(contourMoment['m01'] / contourMoment['m00'])]
                 center = (int(contourMoment['m10'] / contourMoment['m00']), int(contourMoment['m01'] / contourMoment['m00']))
                 dotCenters.append(center)
-                #cv2.circle(image2,center, 1, (255,0,0), -1)
+                cv2.circle(image2,center, 1, (255,0,0), -1)
             except:
                 continue
-    #cv2.imwrite(file, image2)
+    cv2.imwrite(file, image2)
     # x, y
     return dotCenters
 
 
 def pixelWiseScan(img, minPix, maxPix):
-    #image3 = np.zeros((2448,3264,3),np.uint8)
+    image3 = np.zeros((2448,3264,3),np.uint8)
     threshPixel = []
     dotArr = []
     dataFinal = []
@@ -92,7 +92,7 @@ def pixelWiseScan(img, minPix, maxPix):
                 # Check dot area 
                 if (minPix < len(dotArr) < maxPix):
                     center = getCenter(dotArr)
-                    #cv2.circle(image3,center, 1, (255,0,0), -1)
+                    cv2.circle(image3,center, 1, (255,0,0), -1)
                     dataFinal.append(center)
                     
                 dotArr = []
@@ -102,7 +102,7 @@ def pixelWiseScan(img, minPix, maxPix):
         except:
             continue
 
-    #cv2.imwrite("image4test.png", image3)
+    cv2.imwrite("image4test.png", image3)
 
     # inverts array 
     return dataFinal[::-1]
@@ -128,6 +128,7 @@ def compareImg(img1, img2, thresh):
     imgMean = np.mean(diffArr)
     nonZero = np.nonzero(diffArr)
     imgMean2 = np.mean(nonZero)
+    print(imgMean)
 
 
     if (imgMean < thresh):
